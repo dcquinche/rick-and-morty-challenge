af0c1411e4e2005108ac1ef2008695da12c15c5d
GDiana <dcquinche@gmail.com>
2023-03-04 12:50:41 -0500

    Add Login/Logout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import './styles.css';
+import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightToBracket, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
+  const {
+    loginWithRedirect, logout, isAuthenticated,
+  } = useAuth0();
   const navigate = useNavigate();
 
   const handleClickCharacters = () => {
@@ -24,6 +30,13 @@ const NavBar = () => {
         <p className="navbar__lists" onClick={handleClickCharacters}>Characters</p>
         <p className="navbar__lists" onClick={handleClickEpisodes}>Episodes</p>
         <p className="navbar__lists" onClick={handleClickLocations}>Locations</p>
+        <section className="navbar__lists">
+          { isAuthenticated ? (
+            <FontAwesomeIcon className="navbar__logButton" icon={faRightFromBracket} title="Log Out" onClick={logout} />
+          ) : (
+            <FontAwesomeIcon className="navbar__logButton" icon={faRightToBracket} title="Log In" onClick={loginWithRedirect} />
+          )}
+        </section>
       </section>
     </nav>
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,11 +14,19 @@ const client = new ApolloClient({
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ApolloProvider>,
+  <Auth0Provider
+    domain="coffeecoders-mintic.us.auth0.com"
+    clientId="YcSKTDCfQGId6PK5Lk0WEvFkhTYqV9fy"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </Auth0Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
